2f3b92442ad2c916b06c245b5e2667871b1060d0
macbook <ahmadfauzirahman99@gmail.com>
2019-02-17 20:20:08 +0700

    Pengaturan File

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -17,7 +17,20 @@
 
 <!-- Slimscroll JavaScript -->
 <script src="{{asset('public/dist/js/jquery.slimscroll.js')}}"></script>
-
+<!-- Data Table JavaScript -->
+<script src="{{asset('public/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-dt/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
+<script src="{{asset('public/vendors/jszip/dist/jszip.min.js')}}"></script>
+<script src="{{asset('public/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
+<script src="{{asset('public/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('public/vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('public/dist/js/dataTables-data.js')}}"></script>
 <!-- Fancy Dropdown JS -->
 <script src="{{asset('public/dist/js/dropdown-bootstrap-extended.js')}}"></script>
 
