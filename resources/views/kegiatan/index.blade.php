c55683b2bbe287e52b55cba181862aaad6c1f6af
macbook <ahmadfauzirahman99@gmail.com>
2019-02-17 22:14:14 +0700

    Penambahan File modal

--- resources/views/kegiatan/index.blade.php
+++ resources/views/kegiatan/index.blade.php
@@ -9,13 +9,71 @@
 @extends('layouts.main')
 @section('main')
     <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    adsda
+        <div class="col-xl-12">
+            <section class="hk-sec-wrapper">
+                <h5 class="hk-sec-title">Data Kegiatan</h5>
+                <p class="mb-40">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Tambah Data
+                    </button>
+                </p>
+                <div class="row">
+                    <div class="col-sm">
+                        <div class="table-wrap">
+                            <table id="datable_1" class="table table-hover w-100 display pb-30">
+                                <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Acara</th>
+                                    <th>Waktu</th>
+                                    <th>Tempat</th>
+                                    <th>Penyelenggara</th>
+                                    <th>Pengisi</th>
+                                    <th>Keterangan</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php $no = 1; ?>
+                                @foreach($kegiatan as $kegiatans)
+                                    <tr>
+                                        <td><?= $no ?></td>
+                                        <td><?= $kegiatans->kegNamaAcara ?></td>
+                                        <td><?= $kegiatans->kegWaktu ?></td>
+                                        <td><?= $kegiatans->kegTempat ?></td>
+                                        <td><?= $kegiatans->kegPenyelenggara ?></td>
+                                        <td><?= $kegiatans->kegPengisi ?></td>
+                                        <td><?= $kegiatans->kegKeterangan ?></td>
+                                    </tr>
+                                    <?php $no++; ?>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     </div>
 
 @endsection
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Form Modal Kegiatan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Woohoo, you're reading this text in a modal!</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
