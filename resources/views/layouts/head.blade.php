530bcb190c7578923afc63e34aa8436c6a289789
macbook <ahmadfauzirahman99@gmail.com>
2019-02-17 20:20:18 +0700

    Pengaturan File

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -19,6 +19,9 @@
 <link href="{{'public/vendors/morris.js/morris.css'}}" rel="stylesheet" type="text/css"/>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+
+<link href="{{asset('public/vendors/datatables.net-dt/css/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('public/vendors/datatables.net-responsive-dt/css/responsive.dataTables.min.css')}}" rel="stylesheet" type="text/css" />
 <!-- Toggles CSS -->
 <link href="{{asset('public/vendors/jquery-toggles/css/toggles.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('public/vendors/jquery-toggles/css/themes/toggles-light.css')}}" rel="stylesheet"
