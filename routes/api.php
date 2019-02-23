522ff280ccc5382dd698ccff0a1a38e7d2e7db5b
macbook <ahmadfauzirahman99@gmail.com>
2019-02-23 12:01:55 +0700

    Pengaturan File

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('v1')->namespace('Api')->group(function () {
+    Route::get('daftar-kegiatan', 'KegiatanController@showKegiatan');
 });
